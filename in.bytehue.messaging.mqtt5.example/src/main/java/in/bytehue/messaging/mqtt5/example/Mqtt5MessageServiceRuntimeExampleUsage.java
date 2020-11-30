b3ee01e5154d73646db5551911b09ca98aeb7005
Amit Kumar Mondal <admin@amitinside.com>
2020-11-30 15:08:41 +0100

    Added Message Service Runtime Usage Example

---
+++ in.bytehue.messaging.mqtt5.example/src/main/java/in/bytehue/messaging/mqtt5/example/Mqtt5MessageServiceRuntimeExampleUsage.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2020 Amit Kumar Mondal
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package in.bytehue.messaging.mqtt5.example;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.felix.service.command.annotations.GogoCommand;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.messaging.dto.MessagingRuntimeDTO;
+import org.osgi.service.messaging.runtime.MessageServiceRuntime;
+
+@Component(service = Object.class)
+@GogoCommand(function = "runtime", scope = "test")
+public final class Mqtt5MessageServiceRuntimeExampleUsage {
+
+    @Reference
+    private MessageServiceRuntime runtime;
+
+    public String runtime() {
+        final MessagingRuntimeDTO runtimeDTO = runtime.getRuntimeDTO();
+        return ReflectionToStringBuilder.toString(runtimeDTO, ToStringStyle.JSON_STYLE);
+    }
+
+}
