abaa30f0e612935fbc1b47af7df115f90e761832
Amit Mondal <amit-kumar.mondal@telekom.de>
2021-02-08 15:51:41 +0100

    Revert "Converted internal final classes to abstract"
    This reverts commit c69fa5f330fa363b7fcff6a000a17e24d3f5cc5d.

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttMessageConstants.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttMessageConstants.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2021 Amit Kumar Mondal
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -58,7 +58,11 @@ public final class MqttMessageConstants {
     /**
      * Defines standard constants for the MQTT messaging extension features
      */
-    public abstract static class Extension {
+    public static final class Extension {
+
+        private Extension() {
+            throw new IllegalAccessError(NON_INSTANTIABLE);
+        }
 
         /**
          * The name of the {@code Message Expiry Interval} extension of the MQTT 5.0 specification.
@@ -106,7 +110,11 @@ public final class MqttMessageConstants {
     /**
      * Defines standard constants for the MQTT messaging configuration PIDs
      */
-    public abstract static class ConfigurationPid {
+    public static final class ConfigurationPid {
+
+        private ConfigurationPid() {
+            throw new IllegalAccessError(NON_INSTANTIABLE);
+        }
 
         /**
          * The configuration PID to configure the internal executor in Reply-To publisher
