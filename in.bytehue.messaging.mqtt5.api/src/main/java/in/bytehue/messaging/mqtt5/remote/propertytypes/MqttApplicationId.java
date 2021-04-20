d6882fc8ca7640dda796298dfec01306160dd072
Amit Kumar Mondal <admin@amitinside.com>
2021-04-20 20:59:03 +0200

    Fixed Javadoc

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/propertytypes/MqttApplicationId.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/propertytypes/MqttApplicationId.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2021 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -26,8 +26,8 @@ import org.osgi.service.component.annotations.ComponentPropertyType;
  * </ul>
  *
  * <p>
- * This annotation can be used on a {@link Component} to declare the values of
- * the Message Feature.
+ * This annotation can be used on a {@link Component} to declare the id of
+ * the application.
  *
  * @see "Component Property Types"
  */
