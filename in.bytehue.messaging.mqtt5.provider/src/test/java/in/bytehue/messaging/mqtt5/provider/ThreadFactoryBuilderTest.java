ace1c08380193c5ebfd80d1553c6783ceff516a6
Amit Kumar Mondal <admin@amitinside.com>
2023-10-24 19:56:15 +0200

    Applied formatting

--- in.bytehue.messaging.mqtt5.provider/src/test/java/in/bytehue/messaging/mqtt5/provider/ThreadFactoryBuilderTest.java
+++ in.bytehue.messaging.mqtt5.provider/src/test/java/in/bytehue/messaging/mqtt5/provider/ThreadFactoryBuilderTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2020-2023 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -41,10 +41,9 @@ public class ThreadFactoryBuilderTest {
 		};
 
 		final ThreadFactoryBuilder builder = new ThreadFactoryBuilder();
-
 		final ThreadFactory threadFactory = builder.build();
-
 		final Thread thread = threadFactory.newThread(doNothing);
+
 		assertNotNull("Thread must not be null", thread);
 	}
 
@@ -56,7 +55,6 @@ public class ThreadFactoryBuilderTest {
 
 		final ThreadFactoryBuilder builder = new ThreadFactoryBuilder() //
 				.setThreadFactoryName(factoryName);
-
 		final ThreadFactory threadFactory = builder.build();
 
 		for (int i = 0; i < 100; i++) {
