7f7d9c1007eb6bf8d61c4b913d79b4612edbb4c8
Amit Kumar Mondal <admin@amitinside.com>
2024-01-02 13:28:36 +0100

    Updated Copyright Year

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/annotations/package-info.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/annotations/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2020-2023 Amit Kumar Mondal
+ * Copyright 2020-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
