f49f0020d26298b44a77f0a6322f82e8d62950e7
Amit Kumar Mondal <admin@amitinside.com>
2024-05-27 10:22:58 +0200

    Updated MQTT Application Javadoc

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/api/MqttApplication.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/remote/api/MqttApplication.java
@@ -1,74 +1,66 @@
 /*******************************************************************************
- * Copyright 2020-2024 Amit Kumar Mondal
+ * 
+ * © 2020-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
+ * 
  ******************************************************************************/
 package in.bytehue.messaging.mqtt5.remote.api;
 
 import org.osgi.annotation.versioning.ConsumerType;
 import org.osgi.service.messaging.Message;
-
 import in.bytehue.messaging.mqtt5.api.MqttMessageContextBuilder;
 
 /**
- * This interface {@link MqttApplication} should be implemented by consumers to
- * leverage remote resource management
+ * The {@link MqttApplication} interface should be implemented by consumers to
+ * manage remote resources effectively.
  *
  * <p>
  * Every application must provide the following service property:
- * <p>
- * <b>mqtt.application.id</b> - that will be used to identify this application
+ * <b>mqtt.application.id</b> - This property identifies the application.
+ * </p>
  *
  * <ul>
  * <li>{@link MqttApplication#doPUT(String, Message, MqttMessageContextBuilder)}
- * is used to implement a CREATE request for a resource identified in the
- * supplied {@link Message}</li>
+ * implements a CREATE request for a resource identified in the supplied {@link Message}.</li>
  * <li>{@link MqttApplication#doGET(String, Message, MqttMessageContextBuilder)}
- * is used to implement a READ request for a resource identified in the supplied
- * {@link Message)}</li>
+ * implements a READ request for a resource identified in the supplied {@link Message}.</li>
  * <li>{@link MqttApplication#doPOST(String, Message, MqttMessageContextBuilder)}
- * is used to implement an UPDATE request for a resource identified in the
- * supplied {@link Message}</li>
+ * implements an UPDATE request for a resource identified in the supplied {@link Message}.</li>
  * <li>{@link MqttApplication#doDELETE(String, Message, MqttMessageContextBuilder)}
- * is used to implement a DELETE request for a resource identified in the
- * supplied {@link Message}</li>
+ * implements a DELETE request for a resource identified in the supplied {@link Message}.</li>
  * <li>{@link MqttApplication#doEXEC(String, Message, MqttMessageContextBuilder)}
- * is used to perform application operation not necessary tied to a given
- * resource.</li>
+ * performs application operations not necessarily tied to a specific resource.</li>
  * </ul>
  *
  * @since 1.0
  */
 @ConsumerType
-// @formatter:off
 public interface MqttApplication {
 
     /**
-     * The service property denoting the application identifier
+     * The service property denoting the application identifier.
      */
     String APPLICATION_ID_PROPERTY = "mqtt.application.id";
 
     /**
-     * Callback for a READ request for a resource
+     * Handles a READ request for a resource.
      *
      * @param resource the resource identifier
      * @param requestMessage the received message
-     * @param messageBuilder the builder to build the response message
-     *
-     * @return the response to be provided back as {@link Message}
-     * @throws Exception
-     *             An exception is thrown in every condition where the request cannot be full fitted due to wrong
-     *             request parameters or exceptions during processing
+     * @param messageBuilder the builder to construct the response message
+     * @return the response message as {@link Message}
+     * @throws Exception if the request cannot be fulfilled due to invalid parameters or processing errors
      */
     default Message doGET(
             final String resource,
@@ -78,16 +70,13 @@ public interface MqttApplication {
     }
 
     /**
-     * Callback for a CREATE request for a resource
+     * Handles a CREATE request for a resource.
      *
      * @param resource the resource identifier
      * @param requestMessage the received message
-     * @param messageBuilder the builder to build the response message
-     *
-     * @return the response to be provided back as {@link Message}
-     * @throws Exception
-     *             An exception is thrown in every condition where the request cannot be full fitted due to wrong
-     *             request parameters or exceptions during processing
+     * @param messageBuilder the builder to construct the response message
+     * @return the response message as {@link Message}
+     * @throws Exception if the request cannot be fulfilled due to invalid parameters or processing errors
      */
     default Message doPUT(
             final String resource,
@@ -97,16 +86,13 @@ public interface MqttApplication {
     }
 
     /**
-     * Callback for an UPDATE request for a resource
+     * Handles an UPDATE request for a resource.
      *
      * @param resource the resource identifier
      * @param requestMessage the received message
-     * @param messageBuilder the builder to build the response message
-     *
-     * @return the response to be provided back as {@link Message}
-     * @throws Exception
-     *             An exception is thrown in every condition where the request cannot be full fitted due to wrong
-     *             request parameters or exceptions during processing
+     * @param messageBuilder the builder to construct the response message
+     * @return the response message as {@link Message}
+     * @throws Exception if the request cannot be fulfilled due to invalid parameters or processing errors
      */
     default Message doPOST(
             final String resource,
@@ -116,16 +102,13 @@ public interface MqttApplication {
     }
 
     /**
-     * Callback for a DELETE request for a resource
+     * Handles a DELETE request for a resource.
      *
      * @param resource the resource identifier
      * @param requestMessage the received message
-     * @param messageBuilder the builder to build the response message
-     *
-     * @return the response to be provided back as {@link Message}
-     * @throws Exception
-     *             An exception is thrown in every condition where the request cannot be full fitted due to wrong
-     *             request parameters or exceptions during processing
+     * @param messageBuilder the builder to construct the response message
+     * @return the response message as {@link Message}
+     * @throws Exception if the request cannot be fulfilled due to invalid parameters or processing errors
      */
     default Message doDELETE(
             final String resource,
@@ -135,16 +118,13 @@ public interface MqttApplication {
     }
 
     /**
-     * Callback to perform application operation not necessary tied to a given resource
+     * Performs application operations not necessarily tied to a specific resource.
      *
      * @param resource the resource identifier
      * @param requestMessage the received message
-     * @param messageBuilder the builder to build the response message
-     *
-     * @return the response to be provided back as {@link Message}
-     * @throws Exception
-     *             An exception is thrown in every condition where the request cannot be full fitted due to wrong
-     *             request parameters or exceptions during processing
+     * @param messageBuilder the builder to construct the response message
+     * @return the response message as {@link Message}
+     * @throws Exception if the request cannot be fulfilled due to invalid parameters or processing errors
      */
     default Message doEXEC(
             final String resource,
