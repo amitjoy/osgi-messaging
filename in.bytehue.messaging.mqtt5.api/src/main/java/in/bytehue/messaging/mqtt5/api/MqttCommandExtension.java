e1b5b4797c65d9e9ba17fb7853de071ceca94f54
Amit Kumar Mondal <admin@amitinside.com>
2024-05-31 10:42:15 +0200

    Updated MQTT Command Extension

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttCommandExtension.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttCommandExtension.java
@@ -2,14 +2,14 @@
  * Copyright 2020-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
@@ -18,26 +18,26 @@ package in.bytehue.messaging.mqtt5.api;
 import org.osgi.annotation.versioning.ConsumerType;
 
 /**
- * {@link MqttCommandExtension} is primarily used to provide more information to
- * be displayed in MQTT Gogo command.
+ * The {@link MqttCommandExtension} interface is used to provide additional
+ * information for display in MQTT Gogo commands.
  *
  * @since 1.0
  */
 @ConsumerType
 public interface MqttCommandExtension {
 
-	/**
-	 * Returns the row name to be displayed in the command
-	 *
-	 * @return the row name
-	 */
-	String rowName();
+    /**
+     * Retrieves the name of the row to be displayed in the command.
+     *
+     * @return the row name
+     */
+    String rowName();
 
-	/**
-	 * Returns the row value to be disaplyed in the command
-	 *
-	 * @return the row value
-	 */
-	String rowValue();
+    /**
+     * Retrieves the value of the row to be displayed in the command.
+     *
+     * @return the row value
+     */
+    String rowValue();
 
 }
