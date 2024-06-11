df7cea8c07e00a45de0af2d6e73e2cf330560c08
Amit Kumar Mondal <admin@amitinside.com>
2024-06-11 11:29:47 +0200

    Added missing comments to Message Publisher

--- org.osgi.service.messaging/src/main/java/org/osgi/service/messaging/MessagePublisher.java
+++ org.osgi.service.messaging/src/main/java/org/osgi/service/messaging/MessagePublisher.java
@@ -2,33 +2,36 @@ package org.osgi.service.messaging;
 
 import org.osgi.annotation.versioning.ProviderType;
 
+/**
+ * A service interface for publishing messages to specific topics.
+ * This interface allows publishing messages with or without specifying a target topic
+ * and provides flexibility to override message context settings.
+ */
 @ProviderType
 public interface MessagePublisher {
 
-	/**
-	 * Publish the given {@link Message} to the given topic contained in the message
-	 * context of the message
-	 *
-	 * @param message the {@link Message} to publish
-	 */
-	void publish(Message message);
+    /**
+     * Publishes the given {@link Message} to the topic specified in the message's context.
+     *
+     * @param message the {@link Message} to publish
+     */
+    void publish(Message message);
 
-	/**
-	 * Publish the given {@link Message} to the given topic
-	 *
-	 * @param message the {@link Message} to publish
-	 * @param channel the topic to publish the message to
-	 */
-	void publish(Message message, String channel);
+    /**
+     * Publishes the given {@link Message} to the specified topic.
+     *
+     * @param message the {@link Message} to publish
+     * @param channel the topic to publish the message to
+     */
+    void publish(Message message, String channel);
 
-	/**
-	 * Publish the given {@link Message} using the given {@link MessageContext}. The
-	 * context parameter will override all context information, that come with the
-	 * message’s Message#getContext information
-	 *
-	 * @param message the {@link Message} to send
-	 * @param context the {@link MessageContext} to be used
-	 */
-	void publish(Message message, MessageContext context);
+    /**
+     * Publishes the given {@link Message} using the specified {@link MessageContext}.
+     * This context parameter overrides any context information provided by the message itself.
+     *
+     * @param message the {@link Message} to publish
+     * @param context the {@link MessageContext} to be used for publishing
+     */
+    void publish(Message message, MessageContext context);
 
 }
