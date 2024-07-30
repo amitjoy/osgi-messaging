91d2f836072c6c7b9c8e464f4ee20de50bdfc9f0
Amit Kumar Mondal <admin@amitinside.com>
2024-07-30 11:37:44 +0200

    Restored sleep for context switch

--- in.bytehue.messaging.mqtt5.provider/src/main/java/in/bytehue/messaging/mqtt5/provider/helper/InterruptSafe.java
+++ in.bytehue.messaging.mqtt5.provider/src/main/java/in/bytehue/messaging/mqtt5/provider/helper/InterruptSafe.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2020-2024 Amit Kumar Mondal
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
 package in.bytehue.messaging.mqtt5.provider.helper;
 
 import java.util.concurrent.Callable;
@@ -48,8 +33,8 @@ public final class InterruptSafe {
         Thread.currentThread().setPriority(Math.min(originalPriority + 1, Thread.MAX_PRIORITY));
 
         try {
-            // Yield to other threads if necessary
-            Thread.yield();
+            // Allow context switch by sleeping for 0 milliseconds
+            Thread.sleep(0);
             
             // Execute the task
             return task.call();
