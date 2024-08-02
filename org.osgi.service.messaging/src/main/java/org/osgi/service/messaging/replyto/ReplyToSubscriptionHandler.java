dce333d4ba37eafa984615599cf994566ac307a3
Amit Kumar Mondal <admin@amitinside.com>
2024-08-02 11:44:27 +0200

    Added missing comments to ReplyToSubscriptionHandler

--- org.osgi.service.messaging/src/main/java/org/osgi/service/messaging/replyto/ReplyToSubscriptionHandler.java
+++ org.osgi.service.messaging/src/main/java/org/osgi/service/messaging/replyto/ReplyToSubscriptionHandler.java
@@ -2,12 +2,18 @@ package org.osgi.service.messaging.replyto;
 
 import org.osgi.service.messaging.Message;
 
+/**
+ * The {@code ReplyToSubscriptionHandler} interface defines a handler for processing
+ * incoming response messages in a reply-to messaging pattern. Implementations of this 
+ * interface are responsible for handling and processing the {@link Message} received 
+ * as a response to a previously sent request.
+ */
 public interface ReplyToSubscriptionHandler {
 
 	/**
-	 * Just handles the incoming request {@link Message}.
+	 * Handles the incoming response {@link Message}.
 	 *
-	 * @param requestMessage the {@link Message}
+	 * @param requestMessage the {@link Message} to be processed
 	 */
 	void handleResponse(Message requestMessage);
 
