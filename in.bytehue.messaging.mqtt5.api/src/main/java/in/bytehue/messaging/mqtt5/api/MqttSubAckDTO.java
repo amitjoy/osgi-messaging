2d37122172cbd8ffba6301e02ec4bae1ed72f6cd
Amit Kumar Mondal <admin@amitinside.com>
2025-10-08 11:55:02 +0200

    docs: Add Javadoc to MqttSubAckDTO

--- in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttSubAckDTO.java
+++ in.bytehue.messaging.mqtt5.api/src/main/java/in/bytehue/messaging/mqtt5/api/MqttSubAckDTO.java
@@ -2,11 +2,20 @@ package in.bytehue.messaging.mqtt5.api;
 
 import org.osgi.dto.DTO;
 
+/**
+ * MQTT 5 SubAck message. This message is translated from and to an MQTT 5
+ * SUBACK packet.
+ */
 public class MqttSubAckDTO extends DTO {
-	
+
+	/**
+	 * The Reason Codes of this SubAck message, each belonging to a MQTT subscription
+	 */
 	public String reason;
 
+	/**
+	 * The optional reason string of this SubAck message
+	 */
 	public int[] reasonCodes;
-	
 
 }
