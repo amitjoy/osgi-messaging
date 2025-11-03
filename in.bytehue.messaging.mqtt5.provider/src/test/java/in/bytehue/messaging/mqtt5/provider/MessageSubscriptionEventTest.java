eee0a827f22fd2fd66f073b54e8d68b019800bad
Amit Kumar Mondal <admin@amitinside.com>
2025-11-03 13:45:08 +0100

    Adds MQTT subscription event tests
    Introduces tests to verify the generation of MQTT subscription events.
    Confirms `ACKED` events are dispatched for successful subscriptions.
    Ensures `NO_ACK` events are sent for failed subscriptions due to invalid topics or timeouts.
    This enhances the robustness and diagnosability of the MQTT subscription mechanism.

---
+++ in.bytehue.messaging.mqtt5.provider/src/test/java/in/bytehue/messaging/mqtt5/provider/MessageSubscriptionEventTest.java
@@ -0,0 +1,155 @@
+package in.bytehue.messaging.mqtt5.provider;
+
+import static in.bytehue.messaging.mqtt5.provider.TestHelper.waitForMqttConnectionReady;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.messaging.MessageSubscription;
+
+import aQute.launchpad.Launchpad;
+import aQute.launchpad.LaunchpadBuilder;
+import aQute.launchpad.Service;
+import aQute.launchpad.junit.LaunchpadRunner;
+import in.bytehue.messaging.mqtt5.api.MqttSubAckDTO.Type;
+
+@RunWith(LaunchpadRunner.class)
+public class MessageSubscriptionEventTest {
+
+	@Service
+	private Launchpad launchpad;
+
+	@Service
+	private MessageSubscription subscriber;
+
+	@Service
+	private ConfigurationAdmin configAdmin;
+
+	@SuppressWarnings("resource")
+	static LaunchpadBuilder builder = new LaunchpadBuilder().bndrun("test.bndrun").export("sun.misc");
+
+	@Before
+	public void setup() {
+		waitForMqttConnectionReady(launchpad);
+	}
+
+	@Test
+	public void test_subscription_acked_event_is_sent() throws Exception {
+		final String topic = "a/b/c";
+		final List<Event> receivedEvents = new CopyOnWriteArrayList<>();
+
+		final EventHandler handler = receivedEvents::add;
+		final ServiceRegistration<EventHandler> registration = registerEventHandler(handler, "mqtt/subscription/*");
+
+		try {
+			subscriber.subscribe(topic);
+			await().atMost(10, SECONDS).until(() -> !receivedEvents.isEmpty());
+
+			assertThat(receivedEvents).hasSize(1);
+			final Event event = receivedEvents.get(0);
+
+			assertThat(event.getTopic()).isEqualTo("mqtt/subscription/ACKED");
+			assertEventProperties(event, topic);
+		} finally {
+			registration.unregister();
+		}
+	}
+
+	@Test
+	public void test_subscription_failed_event_is_sent() throws Exception {
+		final String topic = "#"; // invalid topic
+		final List<Event> receivedEvents = new CopyOnWriteArrayList<>();
+
+		final EventHandler handler = receivedEvents::add;
+		final ServiceRegistration<EventHandler> registration = registerEventHandler(handler, "mqtt/subscription/*");
+
+		try {
+			subscriber.subscribe(topic);
+		} catch (final RuntimeException e) {
+			// expected
+		}
+		await().atMost(5, SECONDS).until(() -> !receivedEvents.isEmpty());
+		registration.unregister();
+
+		assertThat(receivedEvents).hasSize(1);
+		final Event event = receivedEvents.get(0);
+
+		assertThat(event.getTopic()).isEqualTo("mqtt/subscription/NO_ACK");
+		assertEventProperties(event, topic, Type.NO_ACK);
+	}
+
+	@Test
+	public void test_subscription_timeout_event_is_sent() throws Exception {
+		final String topic = "a/b/c";
+		final List<Event> receivedEvents = new CopyOnWriteArrayList<>();
+
+		// set a non-routable IP address to force a timeout
+		final Configuration clientConfig = configAdmin.getConfiguration("in.bytehue.messaging.client", "?");
+		final Dictionary<String, Object> clientProps = new Hashtable<>();
+		clientProps.put("server", "10.255.255.1");
+		clientConfig.update(clientProps);
+
+		// set a very low timeout for the test
+		final Configuration subscriberConfig = configAdmin.getConfiguration("in.bytehue.messaging.subscriber", "?");
+		final Dictionary<String, Object> subscriberProps = new Hashtable<>();
+		subscriberProps.put("timeoutInMillis", 1L);
+		subscriberConfig.update(subscriberProps);
+
+		final EventHandler handler = receivedEvents::add;
+		final ServiceRegistration<EventHandler> registration = registerEventHandler(handler, "mqtt/subscription/*");
+
+		try {
+			subscriber.subscribe(topic);
+		} catch (final RuntimeException e) {
+			// expected
+		}
+		await().atMost(5, SECONDS).until(() -> !receivedEvents.isEmpty());
+		registration.unregister();
+
+		assertThat(receivedEvents).hasSize(1);
+		final Event event = receivedEvents.get(0);
+
+		assertThat(event.getTopic()).isEqualTo("mqtt/subscription/NO_ACK");
+		assertEventProperties(event, topic, Type.NO_ACK);
+
+		// clean up the configuration
+		clientProps.put("server", "broker.emqx.io");
+		clientConfig.update(clientProps);
+		subscriberProps.put("timeoutInMillis", 30_000L);
+		subscriberConfig.update(subscriberProps);
+	}
+
+	private ServiceRegistration<EventHandler> registerEventHandler(final EventHandler handler, final String... topics) {
+		final Dictionary<String, Object> props = new Hashtable<>();
+		props.put(EventConstants.EVENT_TOPIC, topics);
+		return launchpad.getBundleContext().registerService(EventHandler.class, handler, props);
+	}
+
+	private void assertEventProperties(final Event event, final String topic) {
+		assertEventProperties(event, topic, Type.ACKED);
+	}
+
+	private void assertEventProperties(final Event event, final String topic, final Type type) {
+		assertThat(event.getProperty("topic")).isEqualTo(topic);
+		assertThat(event.getProperty("type")).isEqualTo(type);
+		assertThat(event.getProperty("qos")).isInstanceOf(Integer.class);
+		assertThat(event.getProperty("replyTo")).isInstanceOf(Boolean.class);
+		assertThat(event.getProperty("timestamp")).isInstanceOf(Long.class);
+		assertThat(event.getProperty("reasonCodes")).isNotNull();
+	}
+
+}
