acfc6a9670edc8f6bdd0a28651f2daa1b735ae15
Amit Kumar Mondal <admin@amitinside.com>
2026-01-14 07:22:09 +0100

    Fixed Memory Leak on Disconnect

--- in.bytehue.messaging.mqtt5.provider/src/main/java/in/bytehue/messaging/mqtt5/provider/MessageClientProvider.java
+++ in.bytehue.messaging.mqtt5.provider/src/main/java/in/bytehue/messaging/mqtt5/provider/MessageClientProvider.java
@@ -624,6 +624,7 @@ public final class MessageClientProvider implements MqttClient {
 				return;
 			}
 			clientToDisconnect = client;
+			client = null;
 			if (isNormalDisconnection) {
 				reasonCode = NORMAL_DISCONNECTION;
 				reasonDescription = "";
