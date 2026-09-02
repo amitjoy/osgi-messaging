010b8009b3e12f9c70c97377db186f4134819200
Amit Kumar Mondal <admin@amitinside.com>
2026-09-02 16:10:35 +0200

    Create test suite for MqttClient connection status and uptime tracking

---
+++ in.bytehue.messaging.mqtt5.provider/src/test/java/in/bytehue/messaging/mqtt5/provider/MessageClientLifecycleTest.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2020-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package in.bytehue.messaging.mqtt5.provider;
+
+import static in.bytehue.messaging.mqtt5.provider.TestHelper.waitForMqttConnectionReady;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import aQute.launchpad.Launchpad;
+import aQute.launchpad.LaunchpadBuilder;
+import aQute.launchpad.Service;
+import aQute.launchpad.junit.LaunchpadRunner;
+import in.bytehue.messaging.mqtt5.api.MqttClient;
+
+@RunWith(LaunchpadRunner.class)
+public final class MessageClientLifecycleTest {
+
+	@Service
+	private Launchpad launchpad;
+
+	@Service
+	private MqttClient client;
+
+	@SuppressWarnings("resource")
+	static LaunchpadBuilder builder = new LaunchpadBuilder().bndrun("test.bndrun").export("sun.misc");
+
+	@Before
+	public void setup() throws InterruptedException {
+		waitForMqttConnectionReady(launchpad);
+	}
+
+	@Test
+	public void test_client_initial_state_and_uptime() {
+		assertThat(client).isNotNull();
+		assertThat(client.isConnected()).isTrue();
+		assertThat(client.getConnectedTimestamp()).isGreaterThan(0L);
+		assertThat(client.getLastDisconnectReason()).isNull();
+	}
+
+}
