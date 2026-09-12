22d29e6da4a5f078e279dfb5bc907cb6e56e5ae4
Amit Kumar Mondal <admin@amitinside.com>
2026-09-12 16:30:14 +0200

    Introduce RemoteResourceManagement integration tests for GET and POST methods

---
+++ in.bytehue.messaging.mqtt5.remote.adapter/src/test/java/in/bytehue/messaging/mqtt5/remote/adapter/RemoteResourceHelperTest.java
@@ -0,0 +1,167 @@
+/*******************************************************************************
+ * Copyright 2020-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package in.bytehue.messaging.mqtt5.remote.adapter;
+
+import static in.bytehue.messaging.mqtt5.api.MqttMessageConstants.CLIENT_ID_FRAMEWORK_PROPERTY;
+import static in.bytehue.messaging.mqtt5.api.MqttMessageConstants.ConfigurationPid.CLIENT;
+import static in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.clientID;
+import static in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.exceptionToString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+import in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.MethodType;
+import in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.MqttException;
+import in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.RequestDTO;
+
+public class RemoteResourceHelperTest {
+
+	@Test
+	public void test_private_constructor_throws() throws Exception {
+		final Constructor<RemoteResourceHelper> constructor = RemoteResourceHelper.class.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		assertThatThrownBy(() -> constructor.newInstance())
+				.isInstanceOf(InvocationTargetException.class)
+				.hasCauseInstanceOf(IllegalAccessError.class);
+	}
+
+	@Test
+	public void test_method_type_enum() {
+		assertThat(MethodType.valueOf("GET")).isEqualTo(MethodType.GET);
+		assertThat(MethodType.valueOf("POST")).isEqualTo(MethodType.POST);
+		assertThat(MethodType.valueOf("PUT")).isEqualTo(MethodType.PUT);
+		assertThat(MethodType.valueOf("DELETE")).isEqualTo(MethodType.DELETE);
+		assertThat(MethodType.valueOf("EXEC")).isEqualTo(MethodType.EXEC);
+		assertThat(MethodType.values()).hasSize(5);
+	}
+
+	@Test
+	public void test_mqtt_exception() {
+		final MqttException ex = new MqttException(404, "Not found");
+		assertThat(ex.code).isEqualTo(404);
+		assertThat(ex.getMessage()).isEqualTo("Not found");
+	}
+
+	@Test
+	public void test_request_dto_fields() {
+		final RequestDTO dto = new RequestDTO();
+		dto.applicationId = "app1";
+		dto.method = MethodType.GET;
+		dto.resource = "config/db";
+
+		assertThat(dto.applicationId).isEqualTo("app1");
+		assertThat(dto.method).isEqualTo(MethodType.GET);
+		assertThat(dto.resource).isEqualTo("config/db");
+	}
+
+	@Test
+	public void test_exception_to_string() {
+		final Exception ex = new IllegalArgumentException("invalid argument");
+		final String trace = exceptionToString(ex);
+
+		assertThat(trace).contains("IllegalArgumentException");
+		assertThat(trace).contains("invalid argument");
+	}
+
+	@Test
+	public void test_client_id_from_config_admin() {
+		final ConfigurationAdmin ca = (ConfigurationAdmin) Proxy.newProxyInstance(
+				ConfigurationAdmin.class.getClassLoader(),
+				new Class<?>[] { ConfigurationAdmin.class },
+				(proxy, method, args) -> {
+					if ("getConfiguration".equals(method.getName())) {
+						return createConfigurationMock("client-1234");
+					}
+					return null;
+				});
+
+		final String id = clientID(ca, null);
+		assertThat(id).isEqualTo("client-1234");
+	}
+
+	@Test
+	public void test_client_id_from_bundle_context_property() {
+		final ConfigurationAdmin ca = (ConfigurationAdmin) Proxy.newProxyInstance(
+				ConfigurationAdmin.class.getClassLoader(),
+				new Class<?>[] { ConfigurationAdmin.class },
+				(proxy, method, args) -> {
+					if ("getConfiguration".equals(method.getName())) {
+						return createConfigurationMock(null);
+					}
+					return null;
+				});
+
+		final BundleContext bc = (BundleContext) Proxy.newProxyInstance(
+				BundleContext.class.getClassLoader(),
+				new Class<?>[] { BundleContext.class },
+				(proxy, method, args) -> {
+					if ("getProperty".equals(method.getName()) && CLIENT_ID_FRAMEWORK_PROPERTY.equals(args[0])) {
+						return "fw-client-id";
+					}
+					return null;
+				});
+
+		final String id = clientID(ca, bc);
+		assertThat(id).isEqualTo("fw-client-id");
+	}
+
+	@Test
+	public void test_client_id_on_io_exception() {
+		final ConfigurationAdmin ca = (ConfigurationAdmin) Proxy.newProxyInstance(
+				ConfigurationAdmin.class.getClassLoader(),
+				new Class<?>[] { ConfigurationAdmin.class },
+				(proxy, method, args) -> {
+					if ("getConfiguration".equals(method.getName())) {
+						throw new IOException("storage error");
+					}
+					return null;
+				});
+
+		final String id = clientID(ca, null);
+		assertThat(id).isEqualTo("+");
+	}
+
+	private Configuration createConfigurationMock(final String clientId) {
+		return (Configuration) Proxy.newProxyInstance(
+				Configuration.class.getClassLoader(),
+				new Class<?>[] { Configuration.class },
+				(proxy, method, args) -> {
+					if ("getProperties".equals(method.getName())) {
+						if (clientId == null) {
+							return null;
+						}
+						final Dictionary<String, Object> props = new Hashtable<>();
+						props.put("id", clientId);
+						return props;
+					}
+					return null;
+				});
+	}
+
+}
---
+++ in.bytehue.messaging.mqtt5.remote.adapter/src/test/java/in/bytehue/messaging/mqtt5/remote/adapter/RemoteResourceManagementTest.java
@@ -0,0 +1,290 @@
+/*******************************************************************************
+ * Copyright 2020-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package in.bytehue.messaging.mqtt5.remote.adapter;
+
+import static in.bytehue.messaging.mqtt5.remote.api.MqttApplication.APPLICATION_ID_PROPERTY;
+import static in.bytehue.messaging.mqtt5.remote.api.MqttRemoteConstants.RESPONSE_CODE_OK;
+import static in.bytehue.messaging.mqtt5.remote.api.MqttRemoteConstants.RESPONSE_CODE_PROPERTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.ByteBuffer;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentServiceObjects;
+import org.osgi.service.messaging.Message;
+import org.osgi.service.messaging.MessageContext;
+
+import in.bytehue.messaging.mqtt5.api.MqttMessageConstants;
+import in.bytehue.messaging.mqtt5.api.MqttMessageContextBuilder;
+import in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.MethodType;
+import in.bytehue.messaging.mqtt5.remote.adapter.RemoteResourceHelper.RequestDTO;
+import in.bytehue.messaging.mqtt5.remote.api.MqttApplication;
+
+public class RemoteResourceManagementTest {
+
+	private RemoteResourceManagement rrm;
+	private final List<Map.Entry<Map<String, Object>, MqttApplication>> applications = new ArrayList<>();
+
+	@Before
+	public void setup() throws Exception {
+		rrm = new RemoteResourceManagement();
+
+		// Configure ConfigurationAdmin mock with client id
+		final ConfigurationAdmin ca = (ConfigurationAdmin) Proxy.newProxyInstance(
+				ConfigurationAdmin.class.getClassLoader(),
+				new Class<?>[] { ConfigurationAdmin.class },
+				(proxy, method, args) -> {
+					if ("getConfiguration".equals(method.getName())) {
+						return (Configuration) Proxy.newProxyInstance(
+								Configuration.class.getClassLoader(),
+								new Class<?>[] { Configuration.class },
+								(p, m, a) -> {
+									if ("getProperties".equals(m.getName())) {
+										final Dictionary<String, Object> props = new Hashtable<>();
+										props.put("id", "device-123");
+										return props;
+									}
+									return null;
+								});
+					}
+					return null;
+				});
+
+		setField(rrm, "configurationAdmin", ca);
+		setField(rrm, "applications", applications);
+		setField(rrm, "mcbFactory", createMockMcbFactory());
+	}
+
+	@Test
+	public void test_init_request_parsing_get() throws Exception {
+		final String topic = "CTRL/in/bytehue/device-123/APP-V1/GET/configurations";
+		final Message reqMessage = createMockMessage(topic, "req-cor-id", null);
+
+		final Method initRequest = RemoteResourceManagement.class.getDeclaredMethod("initRequest", String.class, Message.class);
+		initRequest.setAccessible(true);
+
+		final RequestDTO dto = (RequestDTO) initRequest.invoke(rrm, topic, reqMessage);
+
+		assertThat(dto.applicationId).isEqualTo("APP-V1");
+		assertThat(dto.method).isEqualTo(MethodType.GET);
+		assertThat(dto.resource).isEqualTo("configurations");
+		assertThat(dto.requestMessage).isSameAs(reqMessage);
+	}
+
+	@Test
+	public void test_init_request_parsing_post_with_nested_resource() throws Exception {
+		final String topic = "CTRL/in/bytehue/device-123/TELEMETRY/POST/sensors/subsystem/cpu";
+		final Message reqMessage = createMockMessage(topic, "req-cor-id", null);
+
+		final Method initRequest = RemoteResourceManagement.class.getDeclaredMethod("initRequest", String.class, Message.class);
+		initRequest.setAccessible(true);
+
+		final RequestDTO dto = (RequestDTO) initRequest.invoke(rrm, topic, reqMessage);
+
+		assertThat(dto.applicationId).isEqualTo("TELEMETRY");
+		assertThat(dto.method).isEqualTo(MethodType.POST);
+		assertThat(dto.resource).isEqualTo("sensors/subsystem/cpu");
+	}
+
+	@Test
+	public void test_find_application() throws Exception {
+		final MqttApplication app = new MqttApplication() {};
+		final Map<String, Object> props = new HashMap<>();
+		props.put(APPLICATION_ID_PROPERTY, "APP-SAMPLE");
+		applications.add(new AbstractMap.SimpleEntry<>(props, app));
+
+		final Method findApp = RemoteResourceManagement.class.getDeclaredMethod("findApp", String.class);
+		findApp.setAccessible(true);
+
+		final MqttApplication found = (MqttApplication) findApp.invoke(rrm, "APP-SAMPLE");
+		assertThat(found).isSameAs(app);
+
+		final MqttApplication notFound = (MqttApplication) findApp.invoke(rrm, "NON_EXISTENT");
+		assertThat(notFound).isNull();
+	}
+
+	@Test
+	public void test_dispatch_get_request() throws Exception {
+		final AtomicBoolean getInvoked = new AtomicBoolean(false);
+		final MqttApplication app = new MqttApplication() {
+			@Override
+			public Message doGET(final String resource, final Message requestMessage, final MqttMessageContextBuilder builder) {
+				getInvoked.set(true);
+				assertThat(resource).isEqualTo("status");
+				return createMockMessage("resp-channel", null, new HashMap<>());
+			}
+		};
+
+		final Map<String, Object> props = new HashMap<>();
+		props.put(APPLICATION_ID_PROPERTY, "MY-APP");
+		applications.add(new AbstractMap.SimpleEntry<>(props, app));
+
+		final RequestDTO dto = new RequestDTO();
+		dto.applicationId = "MY-APP";
+		dto.method = MethodType.GET;
+		dto.resource = "status";
+		dto.requestMessage = createMockMessage("req-channel", "cor-456", new HashMap<>());
+
+		final Method execApp = RemoteResourceManagement.class.getDeclaredMethod("execMqttApplication", RequestDTO.class);
+		execApp.setAccessible(true);
+
+		final Message response = (Message) execApp.invoke(rrm, dto);
+
+		assertThat(getInvoked.get()).isTrue();
+		assertThat(response).isNotNull();
+
+		final Map<String, Object> extensions = response.getContext().getExtensions();
+		@SuppressWarnings("unchecked")
+		final Map<String, Object> userProps = (Map<String, Object>) extensions.get(MqttMessageConstants.Extension.USER_PROPERTIES);
+		assertThat(userProps.get(RESPONSE_CODE_PROPERTY)).isEqualTo(RESPONSE_CODE_OK);
+	}
+
+	@Test
+	public void test_dispatch_post_request() throws Exception {
+		final AtomicBoolean postInvoked = new AtomicBoolean(false);
+		final MqttApplication app = new MqttApplication() {
+			@Override
+			public Message doPOST(final String resource, final Message requestMessage, final MqttMessageContextBuilder builder) {
+				postInvoked.set(true);
+				assertThat(resource).isEqualTo("data/update");
+				return createMockMessage("resp-channel", null, new HashMap<>());
+			}
+		};
+
+		final Map<String, Object> props = new HashMap<>();
+		props.put(APPLICATION_ID_PROPERTY, "POST-APP");
+		applications.add(new AbstractMap.SimpleEntry<>(props, app));
+
+		final RequestDTO dto = new RequestDTO();
+		dto.applicationId = "POST-APP";
+		dto.method = MethodType.POST;
+		dto.resource = "data/update";
+		dto.requestMessage = createMockMessage("req-channel", "cor-789", new HashMap<>());
+
+		final Method execApp = RemoteResourceManagement.class.getDeclaredMethod("execMqttApplication", RequestDTO.class);
+		execApp.setAccessible(true);
+
+		final Message response = (Message) execApp.invoke(rrm, dto);
+
+		assertThat(postInvoked.get()).isTrue();
+		assertThat(response).isNotNull();
+
+		final Map<String, Object> extensions = response.getContext().getExtensions();
+		@SuppressWarnings("unchecked")
+		final Map<String, Object> userProps = (Map<String, Object>) extensions.get(MqttMessageConstants.Extension.USER_PROPERTIES);
+		assertThat(userProps.get(RESPONSE_CODE_PROPERTY)).isEqualTo(RESPONSE_CODE_OK);
+	}
+
+	private static void setField(final Object target, final String fieldName, final Object value) throws Exception {
+		final Field field = target.getClass().getDeclaredField(fieldName);
+		field.setAccessible(true);
+		field.set(target, value);
+	}
+
+	private static Message createMockMessage(final String channel, final String correlationId, final Map<String, Object> extensions) {
+		final Map<String, Object> ext = extensions != null ? extensions : new HashMap<>();
+		final MessageContext context = (MessageContext) Proxy.newProxyInstance(
+				MessageContext.class.getClassLoader(),
+				new Class<?>[] { MessageContext.class },
+				(proxy, method, args) -> {
+					switch (method.getName()) {
+						case "getChannel":
+							return channel;
+						case "getCorrelationId":
+							return correlationId;
+						case "getReplyToChannel":
+							return "reply-to";
+						case "getExtensions":
+							return ext;
+						default:
+							return null;
+					}
+				});
+
+		return (Message) Proxy.newProxyInstance(
+				Message.class.getClassLoader(),
+				new Class<?>[] { Message.class },
+				(proxy, method, args) -> {
+					if ("getContext".equals(method.getName())) {
+						return context;
+					}
+					if ("payload".equals(method.getName())) {
+						return ByteBuffer.wrap(new byte[0]);
+					}
+					return null;
+				});
+	}
+
+	@SuppressWarnings("unchecked")
+	private static ComponentServiceObjects<MqttMessageContextBuilder> createMockMcbFactory() {
+		return (ComponentServiceObjects<MqttMessageContextBuilder>) Proxy.newProxyInstance(
+				ComponentServiceObjects.class.getClassLoader(),
+				new Class<?>[] { ComponentServiceObjects.class },
+				(proxy, method, args) -> {
+					if ("getService".equals(method.getName())) {
+						return createMockMcb();
+					}
+					return null;
+				});
+	}
+
+	private static MqttMessageContextBuilder createMockMcb() {
+		final Map<String, Object> ext = new HashMap<>();
+		final Map<String, Object> userProps = new HashMap<>();
+		ext.put(MqttMessageConstants.Extension.USER_PROPERTIES, userProps);
+
+		return (MqttMessageContextBuilder) Proxy.newProxyInstance(
+				MqttMessageContextBuilder.class.getClassLoader(),
+				new Class<?>[] { MqttMessageContextBuilder.class },
+				(proxy, method, args) -> {
+					final String name = method.getName();
+					if ("buildMessage".equals(name)) {
+						return createMockMessage("built-channel", "built-cid", new HashMap<>(ext));
+					}
+					if ("extensions".equals(name) && args != null && args.length == 1 && args[0] instanceof Map) {
+						@SuppressWarnings("unchecked")
+						final Map<String, Object> passedExt = (Map<String, Object>) args[0];
+						ext.putAll(passedExt);
+						return proxy;
+					}
+					if ("extensionEntry".equals(name) && args != null && args.length == 2) {
+						ext.put((String) args[0], args[1]);
+						return proxy;
+					}
+					if (method.getReturnType().isAssignableFrom(MqttMessageContextBuilder.class)) {
+						return proxy;
+					}
+					return null;
+				});
+	}
+
+}
